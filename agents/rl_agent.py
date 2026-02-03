2abd5afe121c4718448588e100cfd0993e5ac7d0
Arthur LECUREUR <arthur.lecureur@gmail.com>
2026-02-03 12:19:24 +0100

    Add 3 training dqn ppo td3

--- agents/rl_agent.py
+++ agents/rl_agent.py
@@ -1,16 +1,20 @@
+import numpy as np
+import torch
 from stable_baselines3 import PPO,A2C,TD3
 from stable_baselines3.common.noise import NormalActionNoise
-import numpy as np
-from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement
 
 def train_rl_agent_ppo_mlp(env,eval_env, timesteps=2000000,checkpoint_dir=None,criculam_learning=False):
     # model = PPO("MlpPolicy", env, verbose=1,device='cpu', tensorboard_log="./ppo_shepherd_logs/")
     log_dir = f"./logs/ppo/"
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     if checkpoint_dir is not None:
         print(f"Loading checkpoint from {checkpoint_dir}...")
         model=PPO.load(checkpoint_dir,
                 env=env,
+                device=device,
                 n_steps=2048,
                 ent_coef=0.003,
                 learning_rate=3e-4,
@@ -27,6 +31,7 @@ def train_rl_agent_ppo_mlp(env,eval_env, timesteps=2000000,checkpoint_dir=None,c
         print("No checkpoint provided, training from scratch.")
         model = PPO("MlpPolicy",
                     env=env,
+                    device=device,
                     n_steps=2048,
                     ent_coef=0.003,
                     learning_rate=3e-4,
@@ -35,20 +40,46 @@ def train_rl_agent_ppo_mlp(env,eval_env, timesteps=2000000,checkpoint_dir=None,c
                     tensorboard_log=log_dir
                     )
     
-    eval_callback = EvalCallback(eval_env, best_model_save_path=log_dir,
-                                log_path=log_dir, eval_freq=20000,n_eval_episodes=20,
-                                deterministic=True, render=False)
-    model.learn(total_timesteps=timesteps,callback=eval_callback)
+    stop_train_callback = StopTrainingOnNoModelImprovement(
+        max_no_improvement_evals=5, 
+        min_evals=10, 
+        verbose=1
+    )
+    
+    eval_callback = EvalCallback(
+        eval_env, 
+        best_model_save_path=log_dir,
+        log_path=log_dir, 
+        eval_freq=20000,       # Évalue tous les 20k pas
+        n_eval_episodes=20,
+        deterministic=True, 
+        render=False,
+        callback_after_eval=stop_train_callback  # <--- On lie l'arrêt ici
+    )
+    model.learn(total_timesteps=timesteps, callback=eval_callback)
     return model
 
 def train_rl_agent_td3_mlp(env, eval_env, timesteps=5000000,checkpoint_dir=None,criculam_learning=False):
     log_dir = f"./logs/td3/"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-    model = TD3("MlpPolicy", env, verbose=1,tensorboard_log=log_dir, action_noise=action_noise)
-    eval_callback = EvalCallback(eval_env, best_model_save_path=log_dir,
-                                log_path=log_dir, eval_freq=20000,n_eval_episodes=20,
-                                deterministic=True, render=False)
-    model.learn(total_timesteps=timesteps,callback=eval_callback)
+    model = TD3("MlpPolicy", env, verbose=1,device=device, tensorboard_log=log_dir, action_noise=action_noise)
+    stop_train_callback = StopTrainingOnNoModelImprovement(
+        max_no_improvement_evals=5, 
+        min_evals=10, 
+        verbose=1
+    )
+    eval_callback = EvalCallback(
+        eval_env, 
+        best_model_save_path=log_dir,
+        log_path=log_dir, 
+        eval_freq=20000,
+        n_eval_episodes=20,
+        deterministic=True, 
+        render=False,
+        callback_after_eval=stop_train_callback
+    )
+    model.learn(total_timesteps=timesteps, callback=eval_callback)
     return model
 
